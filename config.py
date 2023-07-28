e8c7c65fed96baf67f2c249b3df74747f2d8cb8e
akshaypn <53367685+akshaypn@users.noreply.github.com>
2023-07-28 22:38:52 +0530

    Second Itiration. New Resume. Almost Complete

---
+++ config.py
@@ -0,0 +1,11 @@
+models = ['openai:gpt-3.5-turbo',
+          'openai:gpt-3.5-turbo-16k',
+          'openai:gpt-3.5-turbo-16k-0613',
+          'openai:text-davinci-003',
+          'huggingface:bigscience/bloom',
+          'huggingface:google/flan-t5-xxl',
+          'huggingface:EleutherAI/gpt-neox-20b',
+          'huggingface:OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
+          'huggingface:bigcode/santacoder',
+          'cohere:command-light-nightly',
+          'cohere:command-nightly']
